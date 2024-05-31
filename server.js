70887d0fa156ee6e0cc78cba648c8f7e2701b6f5
vaibhavpatel <vaibhavP@agilebrainsconsulting.com>
2024-05-31 17:22:16 +0530

    new changes

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 app.use("/generate-post", generatePostRoutes);
 
